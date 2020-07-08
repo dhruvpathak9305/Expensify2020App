cb78edb5666e5f46146a3eff7cb84bb5245c2b98
Dhruv Pathak <dhruvpathak9305@gmail.com>
2020-07-09 02:07:44 +0530

    Enzyme(configuration, installing utilities, automating stuff, using wrapper n taking snapshots) Complete

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
-import Header from '../../components/Header';
+import React from "react";
+import { shallow } from "enzyme";
 
-test('should render Header correctly', () => {
-  const renderer = new ReactShallowRenderer();
-  renderer.render(<Header />);
-  expect(renderer.getRenderOutput()).toMatchSnapshot();
+import Header from "../../components/Header";
+
+test("should render Header correctly", () => {
+  const wrapper = shallow(<Header />);
+  expect().toMatchSnapshot();
 });
