ad559c5a7ee90a35caf4af7dbcffe798c7a894eb
Dhruv Pathak <dhruvpathak9305@gmail.com>
2020-07-09 02:37:18 +0530

    Snapshot Testing with Dynamic Components

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,9 +1,8 @@
-import React from "react";
-import { shallow } from "enzyme";
+import React from 'react';
+import { shallow } from 'enzyme';
+import Header from '../../components/Header';
 
-import Header from "../../components/Header";
-
-test("should render Header correctly", () => {
+test('should render Header correctly', () => {
   const wrapper = shallow(<Header />);
-  expect().toMatchSnapshot();
+  expect(wrapper).toMatchSnapshot();
 });
